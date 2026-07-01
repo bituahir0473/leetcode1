33976ea02493abe69db3ea3a2fbe148633ac1e44
bituahir0473 <bitukumaryadaw8132@gmail.com>
2026-07-01 06:21:48 +0000

    Sync LeetCode submission Runtime - 3 ms (32.77%), Memory - 19 MB (99.82%)

---
+++ 0020-valid-parentheses/solution.py
@@ -0,0 +1,13 @@
+class Solution:
+    def isValid(self, s: str) -> bool:
+       st=[]
+       for i in s:
+        if i=="(":
+            st.append(")")
+        elif i=="[":
+            st.append("]")
+        elif i=="{":
+            st.append("}")
+        elif not st or st.pop()!=i:
+            return False
+       return not st
