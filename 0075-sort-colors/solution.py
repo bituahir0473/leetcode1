0e723f0f3d38c7c873ad9b77fe204804c4340050
bituahir0473 <bitukumaryadaw8132@gmail.com>
2026-07-09 04:49:16 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 19.2 MB (60.88%)

---
+++ 0075-sort-colors/solution.py
@@ -0,0 +1,8 @@
+class Solution:
+    def sortColors(self, nums: List[int]) -> None:
+        nums=nums.sort()
+        print(nums)
+        """
+        Do not return anything, modify nums in-place instead.
+        """
+        
