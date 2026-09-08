981a8fe68bd4ef4adb77b09f65de9bc55bc5f1b6
bituahir0473 <bitukumaryadaw8132@gmail.com>
2026-09-08 13:24:26 +0530

    Time: 1 ms (48.10%) | Memory: 19 MB (98.39%) - LeetSync

--- 2844-sum-of-squares-of-special-elements/sum-of-squares-of-special-elements.py
+++ 2844-sum-of-squares-of-special-elements/sum-of-squares-of-special-elements.py
@@ -4,5 +4,6 @@ class Solution:
         s=0
         for i in range(1,n+1):
             if n%i==0:
-                s +=nums[i-1]*nums[i-1]
+                s +=nums[i-1]**2
+                
         return s
