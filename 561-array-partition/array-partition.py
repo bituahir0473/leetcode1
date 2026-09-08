984771a1de0f8c59555c2205b25811a55b1e7a9f
bituahir0473 <bitukumaryadaw8132@gmail.com>
2026-09-08 14:05:23 +0530

    Time: 23 ms (77.86%) | Memory: 21.4 MB (61.44%) - LeetSync

--- 561-array-partition/array-partition.py
+++ 561-array-partition/array-partition.py
@@ -3,7 +3,7 @@ class Solution:
         a=0
         nums.sort()
         n=len(nums)
-        for  i in range(0,n):
-            if i%2==0:
+        for  i in range(0,n,2):
+           
                 a+=nums[i]
         return a
